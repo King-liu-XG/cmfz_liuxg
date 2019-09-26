bd014da28195998ba009ce80ddaf2f9cf1929fe9
liuKing <1842121388@qq.com>
2019-09-26 21:54:39 +0800

    修改文章BUG错误后

--- src/test/java/com/baizhi/TestRedis.java
+++ src/test/java/com/baizhi/TestRedis.java
@@ -4,23 +4,20 @@ package com.baizhi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestRedis {
 
-    @Resource
-    private RedisTemplate redisTemplate;
+//    @Resource
+//    private RedisTemplate redisTemplate;
 
     @Test
     public void test1(){
-        ValueOperations valueOperations = redisTemplate.opsForValue();
-        valueOperations.set("2","3");
+//        ValueOperations valueOperations = redisTemplate.opsForValue();
+//        valueOperations.set("2","3");
 
     }
 }
